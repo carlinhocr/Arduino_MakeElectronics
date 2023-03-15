c8999323f3bd2f7809096107677a689fb38f183b
carlinhocr <carlos.rienzi@gmail.com>
2023-03-15 17:33:51 -0300

    starting nicer dice with led iterator

---
+++ NicerDice/NicerDice.ino
@@ -0,0 +1,19 @@
+int pin = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop() {
+  // ITERATOR to test all output pins
+  // for (int thisPin = 3; thisPin < 7; thisPin++) {
+  //   digitalWrite(thisPin, HIGH);
+  //   delay(1000);
+  //   digitalWrite(thisPin, LOW);
+  // };
+}
