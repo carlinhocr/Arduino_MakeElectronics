b0b159b901f225a10687c66c3693a5703bf0d855
carlinhocr <carlos.rienzi@gmail.com>
2023-03-21 17:10:56 -0300

    starting with Two dice

--- MyDice/MyDice.ino
+++ MyDice/MyDice.ino
@@ -5,14 +5,24 @@
 // diceMiddleLeds=5
 // diceCenterLed=4);
 
-MyDice dice1(2,6,3,5,4);
+//MyDice dice1(2,6,3,5,4);
+MyDice diceGreen(2,7,8,9,10);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+        // pinMode(7, OUTPUT);
+        // pinMode(8, OUTPUT);
+        // pinMode(9, OUTPUT);
+        // pinMode(10, OUTPUT);
+        // digitalWrite(7, HIGH);
+        // digitalWrite(8, HIGH);
+        // digitalWrite(9, HIGH);
+        // digitalWrite(10, HIGH);
 }
 
 void loop() {
   //Serial.println(digitalRead(2));
-  dice1.rollDice();
+  //dice1.rollDice();
+  diceGreen.rollDice();
 }
